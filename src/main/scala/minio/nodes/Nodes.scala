2bbdb886fef876bbfed94740c341bf2b0cc1f9a9
Arnold deVos <adv@bgsig.com>
2020-07-26 02:09:28 +1000

    nodes with builder pattern

--- src/main/scala/minio/nodes/Nodes.scala
+++ src/main/scala/minio/nodes/Nodes.scala
@@ -5,11 +5,29 @@ import api2._
 import scala.concurrent.duration._
 
 /**
-* A node in a dataflow graph.  
+* A node in a dataflow system.  
 */
-trait Node[+E, +A] {
+sealed trait NodeT[+E, +A] {
   def action: IO[E, A]
 }
+
+trait SupervisorT[+E, +A] extends NodeT[Nothing, Unit] with InputT[Supervisory[E, A]]
+
+/**
+* A worker node in a dataflow system.  
+*/
+trait Node[+E, +A](system: System[E, A]) extends NodeT[E, A] {
+  system.registerNode(this)
+}
+
+/**
+ * A supervisor node in a dataflow system.
+ */
+trait Supervisor[+E, +A](system: System[E, A]) extends SupervisorT[E, A] {
+  val input = system.status.take
+  system.registerSupervisor(this)
+}
+
 
 /**
 * A message to a supervisor node.
@@ -22,7 +40,7 @@ enum Supervisory[+E, +A] {
 /**
 * A node that accepts input.  
 */
-trait InputT[A] { this: Node[Any, Any] =>
+trait InputT[+A] { this: NodeT[Any, Any] =>
 
   def input: IO[Nothing, A]
 
@@ -37,104 +55,102 @@ trait InputT[A] { this: Node[Any, Any] =>
 /**
 * A node that accepts an alternative input.  
 */
-trait WyeT[A] {  this: Node[Any, Any] => 
+trait WyeT[+A] {  this: NodeT[Any, Any] => 
   def wye: IO[Nothing, A]
 }
 
 /**
 * A node that produces output.  
 */
-trait OutputT[A] { this: Node[Any, Any] => 
+trait OutputT[-A] { this: NodeT[Any, Any] => 
   def output: A => IO[Nothing, Unit]
 }
 
 /**
 * A node that produces an alternative output.  
 */
-trait TeeT[A] {  this: Node[Any, Any] => 
+trait TeeT[-A] {  this: NodeT[Any, Any] => 
   def tee: A => IO[Nothing, Unit]
 }
 
 /**
  *  Specify an input.
  */
-trait Input[G : In[A], A](g: G) extends InputT[A] { this: Node[Any, Any] =>
+trait Input[G : In[A], A](g: G) extends InputT[A] { this: NodeT[Any, Any] =>
   val input = g.input
 }
 
 /**
  *  Specify a second input.
  */
-trait Wye[G : In[A], A](g: G) extends WyeT[A] { this: Node[Any, Any] =>
+trait Wye[G : In[A], A](g: G) extends WyeT[A] { this: NodeT[Any, Any] =>
   val wye = g.input
 }
 
 /**
  *  Specify an output.
  */
-trait Output[G : Out[A], A](g: G) extends OutputT[A] { this: Node[Any, Any] =>
+trait Output[G : Out[A], A](g: G) extends OutputT[A] { this: NodeT[Any, Any] =>
   val output = g.output
 }
 
 /**
  *  Specify a second output.
  */
-trait Tee[G : Out[A], A](g: G) extends TeeT[A] { this: Node[Any, Any] =>
+trait Tee[G : Out[A], A](g: G) extends TeeT[A] { this: NodeT[Any, Any] =>
   val tee = g.output
 }
 
 /**
- * A supervisor depends on a status input channel.
- */
- type Supervisor[-E, -A] = IO[Nothing, Supervisory[E, A]] => Node[Nothing, Unit]
-
-
-/**
  * The default supervisor node. Interrupts all nodes if one fails.
  */
- val allFailSupervisor: Supervisor[Any, Any] = {
-   status => 
-    new Node[Nothing, Unit] with Input(status) {
-      import Supervisory._
+ trait AllFail extends SupervisorT[Any, Any] {
+  import Supervisory._
 
-      def action = react {
-        case Stopped(_, _, ex) if !ex.succeeded => unit
-        case _ => action
-      }
-    }
- }
+  def action = react {
+    case Stopped(_, _, ex) if !ex.succeeded => unit
+    case _ => action
+  }
+}
 
 /**
  * A system of nodes with a supervisor.
  */
-class System[E, A](supervisor: Supervisor[E, A] = allFailSupervisor)(nodes: => Iterable[Node[E, A]]) { 
+class System[E, A] { 
 
-  def start: IO[Nothing, Unit] = { 
+  import scala.collection.mutable.Buffer
 
-    val status = queue[Supervisory[E, A]](1)
+  val status = queue[Supervisory[E, A]](10)
+  private val nodes = Buffer[Node[E, A]]()
+  private var supervisor: Option[Supervisor[E, A]] = None
 
-    def startNode(node: Node[E, A]): IO[Nothing, Unit] = {
-      import Supervisory._
+  def registerNode(node: Node[E, A]): Unit = nodes += node
+  def registerSupervisor(node: Supervisor[E, A]): Unit = supervisor = Some(node)
 
-      def monitor(fiber: Fiber[E, A]) =
-        for {
-          ex <- fiber.await
-          _  <- status.offer(Stopped(node, fiber, ex))
-        }
-        yield ()
+  private def startNode(node: Node[E, A]): IO[Nothing, Unit] = {
+    import Supervisory._
 
+    def monitor(fiber: Fiber[E, A]) =
       for {
-        fiber <- node.action.fork
-        _     <- status.offer(Started(node, fiber)).fork
-        _     <- monitor(fiber).fork
+        ex <- fiber.await
+        _  <- status.offer(Stopped(node, fiber, ex))
       }
       yield ()
-    }
 
     for {
+      fiber <- node.action.fork
+      _     <- status.offer(Started(node, fiber)).fork
+      _     <- monitor(fiber).fork
+    }
+    yield ()
+  }
+
+
+  def start: IO[Nothing, Unit] = { 
+    val visor = supervisor.getOrElse(new Supervisor(this) with AllFail)
+    for {
       _ <- foreach(nodes)(startNode)
-      s = supervisor(status.take)
-      _ <- s.action
+      _ <- visor.action
     }
     yield ()
   }
@@ -143,7 +159,7 @@ class System[E, A](supervisor: Supervisor[E, A] = allFailSupervisor)(nodes: => I
 /**
  * Specify a name for a node.
  */
-trait Name(name: String) { this: Node[Any, Any] =>
+trait Name(name: String) { this: NodeT[Any, Any] =>
   override def toString  = name
 }
 
--- src/main/scala/minio/playground/example.scala
+++ src/main/scala/minio/playground/example.scala
@@ -4,34 +4,31 @@ package playground
 import api2._
 import nodes._
 
-val exampleSuper: Supervisor[Any, Any] = {
-  status => 
-   new Node[Nothing, Unit] with Input(status) {
-     import Supervisory._
+trait ExamplePolicy extends SupervisorT[Any, Any] {
+  import Supervisory._
 
-     def action = react {
-       case s @ Stopped(_, _, ex) if !ex.succeeded => effectTotal(println(s))
-       case s => effectTotal(println(s)) andThen action
-     }
-   }
+  def action = react {
+    case s @ Stopped(_, _, ex) if !ex.succeeded => effectTotal(println(s))
+    case s => effectTotal(println(s)) andThen action
+  }
 }
 
-val sys = System[Throwable, Unit](exampleSuper) {
-  val stage1, stage2 = queue[String](10)
+val sys = System[Throwable, Unit] 
 
-  List(
-    new Node with Output(stage1) with Name("data source") {
-      def action = output("the message")
-    },
+val stage1, stage2 = queue[String](10)
 
-    new Node with Input(stage1) with Output(stage2) with Name("data enrich") {
-      def action = react { s => output("here it comes...") andThen output(s) andThen action }
-    },
+val sup = new Supervisor(sys) with ExamplePolicy
 
-    new Node with Input(stage2) with Name("data sink") {
-      def action = react { s => effect(println(s"received: $s")) andThen action }
-    },
-  )
+val src = new Node(sys) with Output(stage1) with Name("data source") {
+  def action = output("the message")
+}
+
+val enr = new Node(sys) with Input(stage1) with Output(stage2) with Name("data enrich") {
+  def action = react { s => output("here it comes...") andThen output(s) andThen action }
+}
+
+val snk = new Node(sys) with Input(stage2) with Name("data sink") {
+  def action = react { s => effect(println(s"received: $s")) andThen action }
 }
 
 object ExampleMain extends App {
